ea3ea7b11f76490aa6fe85f22a4c49badcc90735
yulisuo <yulisuo01@qiyi.com>
2022-10-08 11:32:08 +0800

    update OpenGLTest1
    1) fix setContentView
    2) add comment about sample code come from

--- OpenGLTest1/app/src/main/java/com/example/opengltest1/MainActivity.java
+++ OpenGLTest1/app/src/main/java/com/example/opengltest1/MainActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.opengl.GLSurfaceView;
 import android.os.Bundle;
 
+/**
+ * 这个工程来自android的sample app，代码可以见aosp的
+ * development/samples/OpenGL/HelloOpenGLES20/
+ */
 public class MainActivity extends AppCompatActivity {
 
     private GLSurfaceView mGLView;
@@ -12,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        /*setContentView(R.layout.activity_main);*/
 
         mGLView = new MyGLSurfaceView(this);
         setContentView(mGLView);
